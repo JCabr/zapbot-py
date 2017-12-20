480ea3be32e6ca5c7e94629985ca546a51335962
J Cabr <ccabral6895997@outlook.com>
2017-12-20 15:54:35 -0700

    Moved files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup\
 (
     name="zapbot",
     packages=["zapbot"], # this must be the same as the name above
-    version="0.3A",
+    version="0.3.1",
     description="A framework for writing Discord bots that is designed to be simple, yet flexible.",
     author="J Cabr",
     author_email="ccabral6895997@outlook.com",
